b5d356468bdacde0caec6da442e196c67629b243
Lei Gong <lgong0715@gmail.com>
2013-05-06 22:07:37 -0700

    revise run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# We are using a special variable that is set by the cluster when a job runs.
-mkdir $PBS_JOBID
-
-# Change to that new directory
-cd $PBS_JOBID
-
-cat python starplus.py >>run.sh
+python starplus.py
