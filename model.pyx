4156a593c75020c5e289674f94053a3deb300896
Lei Gong <lgong0715@gmail.com>
2013-07-01 18:17:09 -0700

    cython debug

--- model.pyx
+++ model.pyx
@@ -172,66 +172,90 @@ cpdef double log_Ising(int j, np.ndarray[double, ndim = 1] theta, np.ndarray[dou
             if gamma[k, j] == gamma[v, j]:
                 s += w(coord, v, k)
     
-#     s = np.sum([w(coord, v, k) for v in xrange(N) for k in neigh[v] if gamma[k, j] == gamma[v, j]])
     output = theta[j]*s
     return output
     
     
 #### MLE for rho ####
 # log likelihood function for AR(1) model
-def loglike_ar(x, y):
-    rho = x[0]
-    sig = x[1]
-    T = len(y)  # number of time point
+def loglike_ar(np.ndarray[double, ndim = 1] x, np.ndarray[double, ndim = 1] y):
+
+    cdef double rho = x[0]
+    cdef double sig = x[1]
+    cdef int T_int = len(y) # number of time point
+    cdef double T = np.float(T_int)
+    cdef double part = 0
+    cdef int i
+
     if rho <= -1 or rho >= 1 or sig <= 0:
         return -np.inf
     else:
-        T = len(y)  # number of time point
-        part = np.sum([(y[i+1]-rho*y[i])**2 for i in xrange(T-1)])
-        return -T*np.log(2*np.pi)/2-(T-1)*np.log(sig*(1-rho**2))/2-part/(2*sig*(1-rho**2))-np.log(sig)/2-y[0]**2/(2*sig)   # log-likelihood
+        for i from 0 <= i < (T_int-1):
+            part += (y[i+1]-rho*y[i])**2.0
+        return -T*np.log(2.0*np.pi)/2.0-(T-1.0)*np.log(sig*(1.0-rho**2.0))/2.0-part/(2.0*sig*(1.0-rho**2.0))-np.log(sig)/2.0-y[0]**2.0/(2.0*sig)   # log-likelihood
 
 # derivative of log likelihood for AR(1) model
-def loglike_ar_der(x, y):   
-    rho = x[0]
-    sig = x[1]
-    T = len(y)  # number of time point
-    der_rho = []
-    der_sig = []
-    part_1 = np.sum([(y[i+1] - rho*y[i])**2 for i in xrange(T-1)])
-    part_2 = np.sum([(y[i+1] - rho*y[i])*y[i] for i in xrange(T-1)])
-    der_rho = (T-1)*rho/(1-rho**2) - rho*part_1/(sig*(1-rho**2)**2) + part_2/(sig*(1-rho**2))
-    der_sig = -(T-1)/(2*sig) + part_1/(2*(1-rho**2)*(sig**2)) - 1/(2*sig) + y[0]**2/(2*(sig**2))
+def loglike_ar_der(np.ndarray[double, ndim = 1] x, np.ndarray[double, ndim = 1] y):   
+
+    cdef double rho = x[0]
+    cdef double sig = x[1]
+    cdef int T_int = len(y) # number of time point
+    cdef double T = np.float(T_int)
+    cdef double part_1 = 0
+    cdef double part_2 = 0
+    cdef double der_rho, der_sig
+    cdef int i
+    
+    for i from 0 <= i < (T_int-1):
+        part_1 += (y[i+1] - rho*y[i])**2.0
+        part_2 += (y[i+1] - rho*y[i])*y[i]
+    
+    der_rho = (T-1.0)*rho/(1.0-rho**2.0) - rho*part_1/(sig*(1.0-rho**2.0)**2.0) + part_2/(sig*(1.0-rho**2.0))
+    der_sig = -(T-1.0)/(2.0*sig) + part_1/(2.0*(1.0-rho**2.0)*(sig**2.0)) - 1.0/(2.0*sig) + y[0]**2.0/(2.0*(sig**2.0))
     return [der_rho, der_sig]
 
-def loglike_ar_hess(x, y):  # hessian matrix of log likelihood for AR(1) model
-    rho = x[0]
-    sig = x[1]
-    T = len(y)  # number of time point
-    hess = np.zeros((2, 2))
-    part_1 = np.sum([(y[i+1]-rho*y[i])**2 for i in xrange(T-1)])
-    part_2 = np.sum([(y[i+1]-rho*y[i])*y[i] for i in xrange(T-1)])
-    part_3 = np.sum([y[i]**2 for i in xrange(T-1)])
-    hess[0, 0] = (T-1)*(1+rho**2)/((1-rho**2)**2) - (1+3*rho**2)*part_1/(sig*(1-rho**2)**3) + 4*rho*part_2/(sig*(1-rho**2)**2) - part_3/(sig*(1-rho**2))
-    hess[0, 1] = rho*part_1/(((1-rho**2)**2)*(sig**2)) - part_2/((sig**2)*(1-rho**2))
+def loglike_ar_hess(np.ndarray[double, ndim = 1] x, np.ndarray[double, ndim = 1] y):  # hessian matrix of log likelihood for AR(1) model
+    
+    cdef double rho = x[0]
+    cdef double sig = x[1]
+    cdef int T_int = len(y) # number of time point
+    cdef double T = np.float(T_int)
+    cdef double part_1 = 0.0
+    cdef double part_2 = 0.0
+    cdef double part_3 = 0.0
+    cdef int i
+    cdef np.ndarray[double, ndim = 2] hess = np.zeros((2, 2))
+
+    for i from 0 <= i < (T_int-1):
+        part_1 += (y[i+1]-rho*y[i])**2.0
+        part_2 += (y[i+1]-rho*y[i])*y[i]
+        part_3 += y[i]**2.0
+    
+    hess[0, 0] = (T-1.0)*(1.0+rho**2.0)/((1.0-rho**2.0)**2.0) - (1.0+3.0*rho**2.0)*part_1/(sig*(1.0-rho**2.0)**3.0) + 4.0*rho*part_2/(sig*(1.0-rho**2.0)**2.0) - part_3/(sig*(1.0-rho**2.0))
+    hess[0, 1] = rho*part_1/(((1.0-rho**2.0)**2.0)*(sig**2.0)) - part_2/((sig**2.0)*(1.0-rho**2.0))
     hess[1, 0] = hess[0, 1]
-    hess[1, 1] = (T-1)/(2*sig**2) - part_1/((1-rho**2)*(sig**3)) + 1/(2*sig**2) - y[0]**2/(sig**3)
+    hess[1, 1] = (T-1.0)/(2.0*sig**2.0) - part_1/((1.0-rho**2.0)*(sig**3.0)) + 1.0/(2.0*sig**2.0) - y[0]**2.0/(sig**3.0)
     return hess
     
-def Newton(f, f_der, f_hess, x_0, data, eps = 10**(-6)):    # Newton's method
-    f_0 = f(x_0, data)  # benchmark
-    x_0 -= np.dot(inv(f_hess(x_0, data)), f_der(x_0, data))    # update
-    while abs(f(x_0, data) - f_0) > eps:
-        f_0 = f(x_0, data)  # benchmark
-        x_0 -= np.dot(inv(f_hess(x_0, data)), f_der(x_0, data))
-    return x_0
+cpdef np.ndarray[double, ndim = 1] Newton(f, f_der, f_hess, np.ndarray[double, ndim = 1] x_0, np.ndarray[double, ndim = 1] data, double eps = 10**(-6)):    # Newton's method    
+    
+    cdef double f_0 = f(x_0, data)  # benchmark
+    cdef np.ndarray[double, ndim = 1] y_0 = x_0
+    
+#     import pdb; pdb.set_trace() # debug flag
+    y_0 -= np.dot(inv(f_hess(y_0, data)), f_der(y_0, data))    # update
+    while abs(f(y_0, data) - f_0) > eps:
+        f_0 = f(y_0, data)  # benchmark
+        y_0 -= np.dot(inv(f_hess(y_0, data)), f_der(y_0, data))
+    return y_0
     
 # MLE for rho and sigma 
-cpdef np.ndarray[double, ndim = 1] rhosig_mle(np.ndarray[int, ndim = 2] data, int N):
+cpdef np.ndarray[double, ndim = 2] rhosig_mle(np.ndarray[double, ndim = 2] data, int N):
     
     cdef int v
-    cdef np.ndarray[double, ndim = 1] output
-
-    output = np.array([Newton(loglike_ar, loglike_ar_der, loglike_ar_hess, [0,1], data[:, v]) for v from 0 <= v < N])
+    cdef np.ndarray[double, ndim = 2] output
+    
+    output = np.array([Newton(loglike_ar, loglike_ar_der, loglike_ar_hess, np.array([0.0, 1.0]), data[:, v]) for v from 0 <= v < N])
     return output
     
     
