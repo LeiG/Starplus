c1f5225f6c7cc8cd5bae990294af66f8defe6b9f
LeiG <LeiG.inbox@gmail.com>
2014-11-13 09:56:34 -0800

    modify diagnostic

--- posterior.pyx
+++ posterior.pyx
@@ -673,7 +673,7 @@ cpdef int mcmc_ess_update(dict neigh, np.ndarray[double, ndim = 3] cov_m_inv, np
 #### MCMC convergence diagnostic ####
 cpdef int mcmc_diag(dict neigh, np.ndarray[double, ndim = 3] cov_m_inv, np.ndarray[double, ndim = 2] data, double tp, np.ndarray[double, ndim = 2] design_m, int p, int N, bytes dirname):
 
-    cdef int thresh = 50000  # threshold (in batches) for checking mcmcse
+    cdef int thresh = 20000  # total simulation effort
     cdef int burnin = 5000  # burn-in period
     cdef unsigned int n = 1   # start simulation
     cdef int i, v, j
